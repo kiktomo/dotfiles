abe95d176440872b5be2eabba8a3f59d5425a9c0
Tomohiro Onuma <kiktomo@gmail.com>
2022-06-07 08:56:38 +0900

    fix

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-cd ~
-ln -s dotfiles/fish                 $HOME/.config/fish
+mkdir -p ~/.config
+cp -pr ./fish ~/.config/
 
 sudo apt-get update
 sudo apt-get install -y colordiff direnv fish git vim
+sudo chsh -s `which fish`
