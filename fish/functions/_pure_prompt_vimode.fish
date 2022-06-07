e449a9f58a96d0e31fd1553dd6e26902739f9341
Tomohiro Onuma <kiktomo@gmail.com>
2022-06-07 09:14:19 +0900

    fix

--- fish/functions/_pure_prompt_vimode.fish
+++ fish/functions/_pure_prompt_vimode.fish
@@ -1,3 +1,5 @@
 function _pure_prompt_vimode
-    echo (fish_default_mode_prompt)
+    if set --query pure_reverse_prompt_symbol_in_vimode; and test "$pure_reverse_prompt_symbol_in_vimode" = false
+      echo (fish_default_mode_prompt)
+    end
 end
