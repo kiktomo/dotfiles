6284a1d5ed1a0827dcab1c5e1a1633bc21907314
Tomohiro Onuma <kiktomo@gmail.com>
2022-06-10 15:19:35 +0900

    fix

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
+sudo apt-get update
+sudo apt-get install -y colordiff direnv fish git vim fzf ripgrep
 mkdir -p ~/.config
 
 # fish
 cp -pr ./fish ~/.config/
-
-sudo apt-get update
-sudo apt-get install -y colordiff direnv fish git vim
 
 # vscode
 mkdir -p ~/.config/Code/User/
